35e92ee2ce23391dc7c25224c32f31db0bd9eee3
David Pavlic <david.pavlic0@gmail.com>
2025-02-03 17:23:18 +0100

    Refactored MyNavBar.tsx

--- src/components/organisms/MyNavBar.tsx
+++ src/components/organisms/MyNavBar.tsx
@@ -7,72 +7,74 @@ import MyThemeSwitcher from "../atoms/MyThemeSwitcher";
 import MyNavLink from "../molecules/MyNavLink";
 import { FaCog } from "react-icons/fa";
 
+// Define navigation items. Outside of the component to avoid recreating the array on every render.
+const navItems = [
+  { link: "/", title: "Home" },
+  { link: "/projects", title: "Projects" },
+  { link: "/passions", title: "Passions" },
+  { link: "/news-scraper", title: "News Scraper" },
+];
+
+{/* TODO: When the Navbar collapses, the height decreases by a few px */ }
+{/* TODO: When clicking outside of the opened hamburger, close it */}
 export const MyNavBar = () => {
+  // Track window width to conditionally render the settings component.
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+  // Update window width on resize.
   useEffect(() => {
-    const handleResize = () => setWindowWidth(window.innerWidth);
+    // Necessary since add-/removeEventlistener must reference to the same function instance.
+    const handleResize = () => { setWindowWidth(window.innerWidth); };
+    // This will call handleResize whenever the window is resized.
     window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    // It removes the event listener to avoid memory leaks or unnecessary updates.
+    return () => { window.removeEventListener("resize", handleResize); };
   }, []);
 
-  const navItems = [
-    { link: "/", title: "Home" },
-    { link: "/projects", title: "Projects" },
-    { link: "/passions", title: "Passions" },
-    { link: "/news-scraper", title: "News Scraper" },
-  ];
-
   return (
+    //expand md makes it collapse below 767px.
     <Navbar expand="md" className="my-navbar">
-      <Container>
-        <Navbar.Brand href="/" className="my-navbar-brand">
-          David Pavlic
-        </Navbar.Brand>
+      <Navbar.Brand href="/" className="my-navbar-brand">
+        David Pavlic
+      </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      {/* Makes the navbar look good, when the menu collapses */}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />    {/* TODO: what do aria-controls do? */}
 
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="my-navbar-center">
-            {navItems.map((item) => (
-              <MyNavLink key={item.link} title={item.title} link={item.link} />
-            ))}
-          </Nav>
-        </Navbar.Collapse>
+      {/* The id links the collapse to the toggle */}
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="my-navbar-center">
+          {navItems.map((item) => (
+            <MyNavLink title={item.title} link={item.link} /> /* Same property two different params? */
+          ))}
+        </Nav>
+      </Navbar.Collapse>
 
-        {windowWidth <= 991 ? (
-          <Dropdown className="my-navbar-settings-dropdown" align="end" autoClose="outside">
-            <Dropdown.Toggle variant="link" className="my-navbar-settings-icon">
-              <FaCog style={{ color: "black" }} />
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item as="div">
-                <MyThemeSwitcher />
-              </Dropdown.Item>
-              <Dropdown.Item
-                as="div"
-                className="dropdown-container"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <MyLanguageDropDown />
-              </Dropdown.Item>
-              <Dropdown.Item
-                as="div"
-                className="dropdown-container"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <MyEnvDropDown />
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        ) : (
-          <Nav className="my-navbar-settings">
-            <MyThemeSwitcher />
-            <MyLanguageDropDown />
-            <MyEnvDropDown />
-          </Nav>
-        )}
-      </Container>
+      {/* If screensize is below 991px (bootstrap lg) */}
+      {windowWidth <= 991 ? (
+        <Dropdown className="my-navbar-settings-dropdown">
+          <Dropdown.Toggle variant="link" className="my-navbar-settings-icon"> {/* TODO: Remove link and make the button look like hamburger menu */}
+            <FaCog className="my-navbar-settings-icon" />
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={(e) => e.stopPropagation()} > {/* e.stopPropagation makes the menu not close when clicked */}
+              <MyThemeSwitcher />
+            </Dropdown.Item>
+            <Dropdown.Item onClick={(e) => e.stopPropagation()} >
+              <MyLanguageDropDown />
+            </Dropdown.Item>
+            <Dropdown.Item onClick={(e) => e.stopPropagation()} >
+              <MyEnvDropDown />
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      ) : (
+        <Nav className="my-navbar-settings">
+          <MyThemeSwitcher />
+          <MyLanguageDropDown />
+          <MyEnvDropDown />
+        </Nav>
+      )}
     </Navbar>
   );
 };
