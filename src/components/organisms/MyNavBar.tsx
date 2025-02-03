0eae845c853a57b645f73d4c469b69af7fc5c8ff
David Pavlic <david.pavlic0@gmail.com>
2025-02-03 14:22:51 +0100

    added navbar responsive functionality

--- src/components/organisms/MyNavBar.tsx
+++ src/components/organisms/MyNavBar.tsx
@@ -1,11 +1,20 @@
 import "./styling/MyNavBar.css";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Dropdown } from "react-bootstrap";
+import { useState, useEffect } from "react";
 import MyEnvDropDown from "../molecules/MyEnvDropDown";
 import MyLanguageDropDown from "../molecules/MyLanguageDropDown";
 import MyThemeSwitcher from "../atoms/MyThemeSwitcher";
 import MyNavLink from "../molecules/MyNavLink";
+import { FaCog } from "react-icons/fa";
 
 export const MyNavBar = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const navItems = [
     { link: "/", title: "Home" },
@@ -15,25 +24,54 @@ export const MyNavBar = () => {
   ];
 
   return (
-    <Navbar expand="sm" className="my-navbar">
-      <Container className="navbar-container">
-
+    <Navbar expand="md" className="my-navbar">
+      <Container className="navbar-container d-flex justify-content-between align-items-center">
         <Navbar.Brand href="/" className="navbar-brand">
           David Pavlic
         </Navbar.Brand>
 
-        <Nav className="center-nav">
-          {navItems.map((item) => (
-            <MyNavLink title={item.title} link={item.link}/>
-          ))}
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <div className="right-nav">
-          <MyThemeSwitcher />
-          <MyLanguageDropDown />
-          <MyEnvDropDown />
-        </div>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="center-nav">
+            {navItems.map((item) => (
+              <MyNavLink key={item.link} title={item.title} link={item.link} />
+            ))}
+          </Nav>
+        </Navbar.Collapse>
 
+        {windowWidth <= 991 ? (
+          <Dropdown className="right-nav-toggle" align="end" autoClose="outside">
+            <Dropdown.Toggle variant="link" className="settings-icon">
+              <FaCog style={{ color: "black" }} />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item as="div">
+                <MyThemeSwitcher />
+              </Dropdown.Item>
+              <Dropdown.Item
+                as="div"
+                className="dropdown-container"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <MyLanguageDropDown />
+              </Dropdown.Item>
+              <Dropdown.Item
+                as="div"
+                className="dropdown-container"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <MyEnvDropDown />
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          <Nav className="right-nav">
+            <MyThemeSwitcher />
+            <MyLanguageDropDown />
+            <MyEnvDropDown />
+          </Nav>
+        )}
       </Container>
     </Navbar>
   );
