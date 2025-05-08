8b77b9c70d72087c7389542bdc36ce667d363883
David Pavlic <david.pavlic0@gmail.com>
2025-05-08 15:28:36 +0200

    Slight MyProjectsPage refactoring

--- src/pages/MyProjectsPage.tsx
+++ src/pages/MyProjectsPage.tsx
@@ -1,20 +1,8 @@
 import './styling/MyProjectsPage.css';
-import { useState, useEffect } from "react";
-import MyProjectCard from "../components/organisms/projectspage/MyProjectCard";
+import { useState, useEffect } from 'react';
+import MyProjectCard from '../components/organisms/projectspage/MyProjectCard';
 import AddProjectCardForm from '../components/organisms/projectspage/MyAddProjectCardForm';
 import { fetchProjectCards } from '../services/MyProjectCardService';
-
-
-///* TODOS *///
-//Solve ProjectCard TODO
-//Refactor, all css namings, to be unique for each component
-//Password secured CRUD operations
-//Git branching
-
-//Search Field for techstack
-//AddForm foldable
-//Find a way to add multiple languages on creation
-//Update Cards
 
 
 ///* TYPE DEFINITION *///
@@ -45,21 +33,19 @@ export const ProjectsPage = () => {
   };
 
   const handleDelete = (id: string) => {
-    console.log(projectscards)
     setProjectcards(prev => prev.filter(card => card.id !== id));
   };
 
   return (
     <div>
       <AddProjectCardForm onProjectAdded={loadProjectCards} />
-
       {projectscards.map((projectCard, index) => (
         <MyProjectCard
           key={index}
           id={projectCard.id}
           title={projectCard.title}
           description={projectCard.description}
-          imageUrl={projectCard.base64Image ? `data:image/jpeg;base64,${projectCard.base64Image}` : ""}
+          imageUrl={projectCard.base64Image ? `data:image/jpeg;base64,${projectCard.base64Image}` : ''}
           techStack={projectCard.techStacks}
           layout={index % 2 === 0}
           onDelete={handleDelete}
