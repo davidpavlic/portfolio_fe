d43d4ac82de1b69541926be9402b75e6f7f147c1
David Pavlic <david.pavlic0@gmail.com>
2025-05-08 15:24:40 +0200

    Slight MyAddProjectCardForm refactoring

--- src/components/organisms/projectspage/MyAddProjectCardForm.tsx
+++ src/components/organisms/projectspage/MyAddProjectCardForm.tsx
@@ -1,10 +1,11 @@
-import "../styling/MyAddProjectCardForm.css";
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
-import { addProjectCard } from "../../../services/MyProjectCardService";
-import MyProjectFileUpload from "../../molecules/projectspage/MyProjectFileUpload";
-import MyProjectFormField from "../../molecules/projectspage/MyProjectFormField";
-import MyProjectTechStack from "../../molecules/projectspage/MyProjectTechStack";
+import '../styling/MyAddProjectCardForm.css';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { addProjectCard } from '../../../services/MyProjectCardService';
+import MyProjectFileUpload from '../../molecules/projectspage/MyProjectFileUpload';
+import MyProjectFormField from '../../molecules/projectspage/MyProjectFormField';
+import MyProjectTechStack from '../../molecules/projectspage/MyProjectTechStack';
+
 
 ///* FUNCTIONAL COMPONENT *///
 const MyAddProjectCardForm = ({ onProjectAdded }: { onProjectAdded: () => void }) => {
@@ -23,50 +24,37 @@ const MyAddProjectCardForm = ({ onProjectAdded }: { onProjectAdded: () => void }
     // Function to validate the form
     const validateForm = () => {
         const newErrors: Record<string, string> = {
-            ...(formData.title.trim() ? {} : { title: t("projects_form_error_no_title") }),                     // Validate title field
-            ...(formData.description.trim() ? {} : { description: t("projects_form_error_no_description") }),   // Validate description field
-            ...(techStack.length > 0 ? {} : { techStack: t("projects_form_error_no_techstack") }),              // Validate tech stack
+            ...(formData.title.trim() ? {} : { title: t('projects_form_error_no_title') }),                     // Validate title field
+            ...(formData.description.trim() ? {} : { description: t('projects_form_error_no_description') }),   // Validate description field
+            ...(techStack.length > 0 ? {} : { techStack: t('projects_form_error_no_techstack') }),              // Validate tech stack
             ...(formData.file                                                                                   // Validate file
                 ? ['application/pdf', 'image/png', 'image/jpeg'].includes(formData.file.type)
                     ? {}
-                    : { file: t("projects_form_error_invalid_image") }
-                : { file: t("projects_form_error_no_image") })
+                    : { file: t('projects_form_error_invalid_image') }
+                : { file: t('projects_form_error_no_image') })
         };
 
-        // Set errors in state
-        setErrors(newErrors);
-
-        // Return true if there are no errors, false otherwise
-        return !Object.keys(newErrors).length; // Returns true if object is empty, false if it contains errors
+        setErrors(newErrors);                   // Set errors in state
+        return !Object.keys(newErrors).length;  // Returns true if object is empty, false if it contains errors
     };
 
     // Handle form submission
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         if (!validateForm()) return;
+
         setIsSubmitting(true);
-
         const formPayload = new FormData();
-        formPayload.append("title", formData.title);
-        formPayload.append("description", formData.description);
-
-        // Append each tech stack entry individually
-        techStack.forEach((tech) => {
-            formPayload.append("techstacks", tech);
-        });
-
-        if (formData.file) {
-            formPayload.append("image", formData.file);
-        }
+        formPayload.append('title', formData.title);
+        formPayload.append('description', formData.description);
+        techStack.forEach((tech) => { formPayload.append('techstacks', tech); });  // Append each tech stack to the form data
+        if (formData.file) formPayload.append('image', formData.file);
 
         const success = await addProjectCard(formPayload);
-
-
-        // Show success or error message
-        setPopupMessage(success ? ("✅ " + t("projects_form_success")) : ("❌ " + t("projects_form_fail")));
+        setPopupMessage(success ? ('✅ ' + t('projects_form_success')) : ('❌ ' + t('projects_form_fail')));
 
         if (success) {
-            setFormData({ title: "", description: "", file: null, techStack: "" });    // Reset form fields if submission is successful
+            setFormData({ title: '', description: '', file: null, techStack: '' });   // Reset form fields if submission is successful
             setTechStack([]);                                                         // Reset techstack
             onProjectAdded();                                                         // Refresh project list after adding
         }
@@ -76,28 +64,23 @@ const MyAddProjectCardForm = ({ onProjectAdded }: { onProjectAdded: () => void }
     };
 
     return (
-        <div className="form-container">
+        <div className='my-project-card-form-container'>
             <h2>{t('projects_form_title')}</h2>
             <form onSubmit={handleSubmit}>
-                <div className="split-layout">
-                    {/* Left column for text fields */}
-                    <div className="left-column">
+                <div className='my-project-card-split-layout'>
+                    <div className='my-project-card-left-column'>
                         <MyProjectFormField
-                            id="title"
-                            label={t("projects_form_card_title")}
+                            id='title'
+                            label={t('projects_form_card_title')}
                             value={formData.title}
                             onChange={(e) => setFormData({ ...formData, title: e.target.value })}
                             error={errors.title}
                             disabled={isSubmitting}
-                            onKeyDown={(e) => {
-                                if (e.key === 'Enter') {
-                                    e.preventDefault(); // Prevent form submission when pressing Enter in title input
-                                }
-                            }}
+                            onKeyDown={e => e.key === 'Enter' && e.preventDefault()}    // Prevent form submission on Enter key
                         />
                         <MyProjectFormField
-                            id="description"
-                            label={t("projects_form_card_description")}
+                            id='description'
+                            label={t('projects_form_card_description')}
                             value={formData.description}
                             onChange={(e) => setFormData({ ...formData, description: e.target.value })}
                             error={errors.description}
@@ -105,8 +88,8 @@ const MyAddProjectCardForm = ({ onProjectAdded }: { onProjectAdded: () => void }
                             isTextArea
                         />
                         <MyProjectTechStack
-                            id="techStack"
-                            label={t("projects_form_card_techstack")}
+                            id='techStack'
+                            label={t('projects_form_card_techstack')}
                             value={formData.techStack}
                             onChange={(e) => setFormData({ ...formData, techStack: e.target.value })}
                             error={errors.techStack}
@@ -115,9 +98,7 @@ const MyAddProjectCardForm = ({ onProjectAdded }: { onProjectAdded: () => void }
                             setTechStack={setTechStack}
                         />
                     </div>
-
-                    {/* Right column for file upload */}
-                    <div className="right-column">
+                    <div className='my-project-card-right-column'>
                         <MyProjectFileUpload
                             file={formData.file}
                             error={errors.file}
@@ -126,22 +107,17 @@ const MyAddProjectCardForm = ({ onProjectAdded }: { onProjectAdded: () => void }
                         />
                     </div>
                 </div>
-
-                {/* Submit button */}
-                <button type="submit" disabled={isSubmitting} className="submit-button">
-                    {isSubmitting ? t("projects_form_submitting") : t("projects_form_submit")}
+                <button type='submit' disabled={isSubmitting} className='my-project-card-submit-button'>
+                    {isSubmitting ? t('projects_form_submitting') : t('projects_form_submit')}
                 </button>
             </form>
 
             {/* Popup message for feedback */}
-            {popupMessage && (
-                <div className="popup-message">
-                    {popupMessage}
-                </div>
-            )}
+            {popupMessage && <div className='my-project-card-popup-message'>{popupMessage}</div>}
         </div>
     );
 };
+
 
 ///* EXPORT *///
 export default MyAddProjectCardForm;
